343275aac81b174a5b1b4f776a78e29cf9183531
evan684900-star <evan684900@gmail.com>
2026-08-25 22:45:23 +0200

    fix fichier index.js propre

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ client.on('interactionCreate', async interaction => {
   await interaction.deferReply();
 
   try {
-        const res = await fetch(url);
+    const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(text)}&langpair=en|${targetLang}`;
+    const res = await fetch(url);
     const data = await res.json();
 
     console.log('MyMemory response:', JSON.stringify(data));
